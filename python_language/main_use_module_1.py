69b4c41604e7402073473effb32ffafe009242e5
shiluan <shi.luan@hotmail.com>
2018-09-21 16:02:03 -0600

    Add files via upload

---
+++ python_language/main_use_module_1.py
@@ -0,0 +1,12 @@
+from module_1 import person1
+from module_1 import greeting
+
+greeting(person1["name"])
+
+import module_1
+p1 = module_1.Person("Kevin", 38)
+p1.myfunc()
+
+from module_1 import Person
+p1 = Person("Kevin", 38)
+p1.myfunc()
---
+++ python_language/module_1.py
@@ -0,0 +1,16 @@
+def greeting(name):
+  print("Hello, " + name)
+  
+person1 = {
+	"name": "John",
+	"age": 36,
+	"country": "Norway"
+}
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
