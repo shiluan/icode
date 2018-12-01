f153e53c9202c90d9bf3194e4497cdd77af03fcc
shiluan <shi.luan@hotmail.com>
2018-11-30 17:35:41 -0700

    Create accounts.js

---
+++ accounts.js
@@ -0,0 +1,34 @@
+var account = {
+  "chq": {
+    "cibc": 100,
+    "tng":100
+  },
+
+  "sav":{
+    "tng":100
+  },
+
+  "rsp":{
+    "tng":100,
+    "cibc":{
+      "shi": 100,
+      "lu": 100
+    } ,
+    "questrade": 100
+  },
+
+  "tfsa": {
+    "tng": {
+      "shi": 100,
+      "lu" : 100
+    }
+  }
+}
+
+
+// render.1 -> <div id='account_1'>
+$(document).ready(
+  function(){
+    $('#account_1').html('test_result');
+  }
+);
