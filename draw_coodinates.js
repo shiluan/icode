4c122eacf8feb51c4d6101b136f8c0cfe07592c1
shiluan <shi.luan@hotmail.com>
2019-02-12 23:06:22 -0700

    Update draw_coodinates.js

--- draw_coodinates.js
+++ draw_coodinates.js
@@ -1,27 +1,31 @@
-
-class PixelCoods
+function PixelCoods()
 {
-this.xmax = 0;
-this.xmin = 0;
-this.ymax = 0;
-this.ymin = 0;
-
+this.xmax = 0.5*canvas.width;
+this.xmin = -0.5*canvas.width;
+this.ymax = 0.5*canvas.height;
+this.ymin = -0.5*canvas.height;
+return this;
 }
 
-class DataCoods
+function DataCoods(xmin=0, xmax=1, ymin=0, ymax=1)
 {
-this.xmax = 0;
-this.xmin = 0;
-this.ymax = 0;
-this.ymin = 0;
-
+this.xmax = xmax;
+this.xmin = xmin;
+this.ymax = ymax;
+this.ymin = ymin;
+return this;
 }
 
 // scale = data_cood/pixel_cood
-Function Scale(scale_x = 0.1, scale_y = 0.1)
+function Scale()
 {
-this.xscale = scale_x;
-this.yscale = scale_y;
+let pc = new PixelCoods();
+let dc = new DataCoods();
+
+
+this.xscale = (dc.xmax-dc.xmin)/(pc.xmax-pc.xmin);
+this.yscale = (dc.ymax-dc.ymin)/(pc.ymax-pc.ymin);
+
 
 this.to_pixel_x = (data_x) =>{
   return data_x/this.xscale;
@@ -36,11 +40,12 @@ return this;
 }
 
 
-class Function
+function MakeFunction()
 {
 this.npoints = 100;
 this.xstart = 0;
-this.xend = 0;
+this.xend = 1;
+
 
 this.pointxs = [];
 this.pointys = [];
@@ -49,22 +54,29 @@ this.make_func = ()=>{
   var step = (this.xend - this.xstart)/npoints;
   for(var i = 0; i<this.npoints; i++){
     this.pointxs[i] = this.xstart + i*step;
-    this.pointys[i] = Math.Pow(this.pointxs[i],2);
+    this.pointys[i] = Math.pow(this.pointxs[i],2);
 
   }
+  return this;
 }; 
 
 this.draw = (ctx)=>{
-  var scale = Scale(0.1,0.1);
+  var scale = Scale();
   
+ctx.save();
+ctx.translate(300,225);
+
   ctx.beginPath();
-  ctx.moveTo(scale.to_pixel_x(this.pointxs[i]), scale.to_pixel_y(this.pointys[i]));
+  ctx.moveTo(scale.to_pixel_x(this.pointxs[0]), scale.to_pixel_y(this.pointys[0]));
   for(var i = 1; i<this.npoints; i++){
     ctx.lineTo(scale.to_pixel_x(this.pointxs[i]), scale.to_pixel_y(this.pointys[i]));
     
   }
   ctx.stroke();
+ctx.restore();
 
 };
+
+return this;
 
 }
