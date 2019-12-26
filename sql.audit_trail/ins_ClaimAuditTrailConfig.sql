faa8a2a6707da2f56bb4b2ba3f3ce8339d4afb6c
shiluan <shi.luan@hotmail.com>
2019-12-26 13:59:12 -0700

    Update ins_ClaimAuditTrailConfig.sql

--- sql.audit_trail/ins_ClaimAuditTrailConfig.sql
+++ sql.audit_trail/ins_ClaimAuditTrailConfig.sql
@@ -5,8 +5,7 @@
 */
 
 
-
-
+/*
 [ClaimSetup]
 	[ClaimBillingNo]
 	[TreatyId]
@@ -27,15 +26,52 @@
 	[Contestable]
 	[CounselRequired]
 	
-	
-
-
 [ClaimLife]
 	[FirstName]
 	[LastName]
 	[BirthDate]
 	[DeathDate]
 	[CauseOfDeath]
+*/
+
+
+INSERT ClaimAuditTrailConfig 
+	(TableName, FieldName)
+VALUES 
+	('ClaimSetup', 'ClaimBillingNo'),
+	('ClaimSetup', 'TreatyId'),
+	('ClaimSetup', 'ClaimStatus'),
+	('ClaimSetup', 'AssumedCompanyShare'),
+	('ClaimSetup', 'AssumedCompanyShareAmt'),
+	('ClaimSetup', 'StatementCurrencyExchangeRate'),
+
+	('ClaimSetup', 'PolicyNumber'),
+	('ClaimSetup', 'SmokerStatus'),
+	('ClaimSetup', 'CoverageSmokingStatus'),
+	('ClaimSetup', 'CoverageUnderwritingRating'),
+	('ClaimSetup', 'CoverageUnderwritingMethod'),
+	('ClaimSetup', 'CoverageUnderwritingType'),
+	('ClaimSetup', 'ProductType'),
+
+	('ClaimSetup', 'Litigated'),
+	('ClaimSetup', 'Contestable'),
+	('ClaimSetup', 'CounselRequired')
+
+	
+
+INSERT ClaimAuditTrailConfig 
+	(TableName, FieldName)
+VALUES 
+	('ClaimLife', 'FirstName'),
+	('LastName'),
+	('BirthDate'),
+	('DeathDate'),
+	('CauseOfDeath')
+
+
+
+
+
 	
 	
 	
