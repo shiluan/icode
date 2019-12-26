4edca583574e146bace9531754b747ee0190841b
shiluan <shi.luan@hotmail.com>
2019-12-26 15:46:09 -0500

    Update ins_ClaimAuditTrailConfig.sql

--- sql.audit_trail/ins_ClaimAuditTrailConfig.sql
+++ sql.audit_trail/ins_ClaimAuditTrailConfig.sql
@@ -3,3 +3,39 @@
   Initialize ClaimAuditTrailConfig entries
 
 */
+
+
+
+
+[ClaimSetup]
+	[ClaimBillingNo]
+	[TreatyId]
+	[ClaimStatus]
+	[AssumedCompanyShare]
+	[AssumedCompanyShareAmt]
+	[StatementCurrencyExchangeRate]
+	
+	[PolicyNumber]
+	[SmokerStatus]
+	[CoverageSmokingStatus]
+	[CoverageUnderwritingRating]
+	[CoverageUnderwritingMethod]
+	[CoverageUnderwritingType]
+	[ProductType]
+	
+	[Litigated]
+	[Contestable]
+	[CounselRequired]
+	
+	
+
+
+[ClaimLife]
+	[FirstName]
+	[LastName]
+	[BirthDate]
+	[DeathDate]
+	[CauseOfDeath]
+	
+	
+	
